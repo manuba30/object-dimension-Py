085d950dd94e48d460dca8333d68cd7204a7eb28
Manoel <manoelcoelho3003@gmail.com>
2024-09-05 19:26:22 +0200

    the first part of the project, delimiting the curves of a page A4 to prepare for the calculations

---
+++ ObjectMeasurement.py
@@ -0,0 +1,33 @@
+import cv2
+import numpy as np
+import utilities
+
+webcam = False
+path = '1.jpg'
+cap = cv2.VideoCapture(0)
+cap.set(10, 160)
+cap.set(3, 1920)
+cap.set(4, 1080)
+
+while True:
+    if webcam:
+        success, img = cap.read()
+    else:
+        img = cv2.imread(path)
+
+    if img is None:
+        print("Erro ao carregar a imagem!")
+        break
+
+    img = cv2.resize(img, (0, 0), None, 0.3, 0.3)  # Reduz para 30% do tamanho original (ajuste conforme necessário)
+
+    img, finalContours = utilities.getContours(img, showCanny=True, draw=True)
+
+    # Exibe a imagem redimensionada com contornos
+    cv2.imshow('original', img)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
